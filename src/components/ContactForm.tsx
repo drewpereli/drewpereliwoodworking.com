98b2a4cba373f43b9a5c3173e2bf1124f8ee0929
Drew Pereli <drewpereli@gmail.com>
2022-08-31 19:36:49 -0700

    Add error/success message

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -7,9 +7,12 @@ export default function ContactForm({ className }: { className: string }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [submissionState, setSubmissionState] = useState<'idle' | 'pending' | 'success' | 'error'>('idle');
 
   async function onSubmit(e: React.FormEvent) {
     e.preventDefault();
+
+    setSubmissionState('pending');
 
     try {
       const r = await emailjs.send(
@@ -22,8 +25,14 @@ export default function ContactForm({ className }: { className: string }) {
         },
         VITE_EMAILJS_PUBLIC_KEY
       );
+
+      setSubmissionState('success');
+      setName('');
+      setEmail('');
+      setMessage('');
     } catch (error) {
       console.error(error);
+      setSubmissionState('error');
     }
   }
 
@@ -46,6 +55,10 @@ export default function ContactForm({ className }: { className: string }) {
       >
         Send
       </button>
+
+      <div className="h-14">
+        <SubmissionStateSection state={submissionState} />
+      </div>
     </form>
   );
 }
@@ -134,5 +147,107 @@ function InputGroup({
       <Label label={label} htmlFor={id} required={required} />
       <Input id={id} value={value} onInput={onInput} required={required} type={type} />
     </InputGroupWrapper>
+  );
+}
+
+function SubmissionStateSection({ state }: { state: 'idle' | 'pending' | 'success' | 'error' }) {
+  if (state === 'pending')
+    return (
+      <div className="flex items-center justify-center">
+        <LoadingSVG />
+      </div>
+    );
+
+  if (state === 'success')
+    return (
+      <div className="text-green-300 text-center font-bold text-lg">
+        Your message was sent! I'll get back to you as soon as possible.
+      </div>
+    );
+
+  if (state === 'error')
+    return (
+      <div className="text-red-400 text-center font-bold text-lg">
+        There was an error sending your message. Please try again later, or email me directly at
+        <a href="mailto:drew@drewpereliwoodworking.com" target="_blank" className="ml-1 hover:text-red-300 underline">
+          drew@drewpereliwoodworking.com
+        </a>
+        .
+      </div>
+    );
+
+  return null;
+}
+
+function LoadingSVG() {
+  return (
+    <svg width="120" height="30" viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg" fill="#fff">
+      <circle cx="15" cy="15" r="15">
+        <animate
+          attributeName="r"
+          from="15"
+          to="15"
+          begin="0s"
+          dur="0.8s"
+          values="15;9;15"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="fill-opacity"
+          from="1"
+          to="1"
+          begin="0s"
+          dur="0.8s"
+          values="1;.5;1"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <circle cx="60" cy="15" r="9" fill-opacity="0.3">
+        <animate
+          attributeName="r"
+          from="9"
+          to="9"
+          begin="0s"
+          dur="0.8s"
+          values="9;15;9"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="fill-opacity"
+          from="0.5"
+          to="0.5"
+          begin="0s"
+          dur="0.8s"
+          values=".5;1;.5"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <circle cx="105" cy="15" r="15">
+        <animate
+          attributeName="r"
+          from="15"
+          to="15"
+          begin="0s"
+          dur="0.8s"
+          values="15;9;15"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="fill-opacity"
+          from="1"
+          to="1"
+          begin="0s"
+          dur="0.8s"
+          values="1;.5;1"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+    </svg>
   );
 }
