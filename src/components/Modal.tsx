816be79b4defd22c1018c9ac97862c70ac85293e
Drew Pereli <drewpereli@gmail.com>
2022-08-31 21:17:04 -0700

    Update modal style

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,7 +6,7 @@ export function Modal({ onClose, children, title }: { onClose: () => unknown; ch
       className="absolute top-0 left-0 h-screen w-screen bg-white/40 flex items-center justify-center z-20 backdrop-blur"
       onClick={onClose}
     >
-      <div className="bg-black w-full max-w-2xl rounded-lg shadow-xl mx-4" onClick={e => e.stopPropagation()}>
+      <div className="bg-black w-full max-w-2xl rounded shadow-xl mx-4" onClick={e => e.stopPropagation()}>
         <div className="flex items-center justify-between p-4 border-b border-neutral-500">
           <h3 className="font-bold text-2xl">{title}</h3>
 
