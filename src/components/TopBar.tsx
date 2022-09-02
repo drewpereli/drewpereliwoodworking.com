38a84eb2ca6edfe125e9d1f30c856e4a39479ab6
Drew Pereli <drewpereli@gmail.com>
2022-09-01 23:45:28 -0700

    Fix contact button mobile style

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -30,7 +30,7 @@ export default function TopBar() {
       <button
         type="button"
         onClick={() => setShowingContactModal(true)}
-        className="w-full md:w-auto bg-gradient-to-tr from-red-400 to-indigo-900 px-8 py-2 rounded-sm shadow shadow-red-400 transition-transform font-bold hover:-translate-y-px hover:scale-105 active:translate-y-px active:scale-100 text-lg"
+        className="w-full md:w-auto bg-gradient-to-tr from-red-400 to-indigo-900 px-8 py-1 md:py-2 rounded-sm shadow shadow-red-400 transition-transform font-bold hover:-translate-y-px hover:scale-105 active:translate-y-px active:scale-100 md:text-lg"
       >
         GET IN TOUCH
       </button>
